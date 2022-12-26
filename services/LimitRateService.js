8a53acd08652e827d5fc95a4951af70456e4e39d
Mathieu T <mathieu.toulon@free.fr>
2022-12-27 00:19:07 +0100

    ⏱️ Limit rate system
    Création d'un système de limitation d'appel

---
+++ services/LimitRateService.js
@@ -0,0 +1,17 @@
+/*
+ * rateLimit is a closure function taking an callback and a rate
+ * the returned function will call the callback only if the last call was made more than rate milliseconds ago
+ */
+export default function rateLimit( callback , rate ) {
+	return (function( ...args ) {
+		let now = +(new Date());
+		if( ( now - this.timestamp ) >= rate ) {
+			this.timestamp = now;
+			this.callback( ...args );
+		}
+	}).bind({
+		callback,
+		rate,
+		timestamp: +(new Date()) 
+	})
+}
