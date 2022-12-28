d2f5b3bf0eb63dcbd0be0da9162e67d586cc0ae8
Mathieu T <mathieu.toulon@free.fr>
2022-12-28 02:53:39 +0100

    📈 Enhance system error handling

--- services/DataService.js
+++ services/DataService.js
@@ -1,11 +1,23 @@
 import config from '../config.json';// assert {type: 'json'};
 
+class BadResponse extends Error {
+	constructor( code , res ) {
+		super( `[${code}] ${res}` )
+		this.code = code;
+	}
+}
+
 async function fetchBackend( endpoint , param ) {
 	if( ! (param instanceof URLSearchParams) ) param = new URLSearchParams(param);
 
 	const URI = `${config.backend.protocol}://${config.backend.adress}:${config.backend.port}${endpoint}?${param}`
 
-	return fetch(URI).then( res => res.json() );
+	return fetch(URI).then( async res => {
+		if( ~~(res.status/100) == 2 )
+			return res.json();
+		else
+			throw BadResponse( res.status , await res.text() ) 
+	} );
 }
 
 function getStats() {
@@ -29,4 +41,4 @@ function getPlantList( page ) {
 	return fetchBackend(`/species/list`,{page});	
 }
 
-export { getStats , getMap , getPlant , getPlantList };
+export { BadResponse , getStats , getMap , getPlant , getPlantList };
